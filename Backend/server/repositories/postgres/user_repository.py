a4949c45f51d7755ef0ce1f2b59e6339a4414d4f
Hout Manut <huotmanut00@gmail.com>
2025-03-12 16:19:46 +0700

    CAL-75: Begin integrating the db to backend

--- Backend/server/repositories/postgres/user_repository.py
+++ Backend/server/repositories/postgres/user_repository.py
@@ -1,5 +1,3 @@
-from sqlalchemy.orm import Session
-
 from models import UserModel
 from repositories.postgres.base import BaseRepository
 from database.database import SessionLocal
@@ -7,12 +5,36 @@ from database.database import SessionLocal
 class UserRepository(BaseRepository[UserModel]):
     def __init__(self) -> None:
         super().__init__(SessionLocal(), UserModel)
-    
+
     def create(self, data: dict) -> UserModel:
         obj = self.model(data)
         self.db.add(obj)
         self.db.commit()
         self.db.refresh(obj)
         return obj
-    
-    
+
+    def get_all(self) -> list[UserModel]:
+        return self.db.query(self.model).all()
+
+    def get_by_id(self, obj_id: int) -> UserModel | None:
+        return self.db.query(self.model).filter(self.model.id == obj_id).first()
+
+    def update(self,obj_id: int, data: dict) -> UserModel | None:
+        obj = self.get_by_id(obj_id)
+        if obj:
+            for key, value in data.items():
+                setattr(obj, key, value)
+            self.db.commit()
+            self.db.refresh(obj)
+        return obj
+
+    def filter_by(self, **filters) -> list[UserModel]:
+        return self.db.query(self.model).filter_by(**filters).all()
+
+    def delete(self, obj_id: int) -> bool: 
+        obj = self.get_by_id(obj_id)
+        if obj:
+            self.db.delete(obj)
+            self.db.commit()
+            return True
+        return False

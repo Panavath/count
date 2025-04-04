1b97814e987a479875ce35f21622a1524c7277a6
Hout Manut <huotmanut00@gmail.com>
2025-04-04 18:33:51 +0700

    Change the result screen to be different depends on whether its a manual log or scan

--- Frontend/lib/main.dart
+++ Frontend/lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:count_frontend/enums/screen_types.dart';
 import 'package:count_frontend/screens/food_input_result_screen.dart';
 import 'package:count_frontend/screens/scan_screen/scan_food_input.dart';
 import 'package:count_frontend/utility/config.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
-import 'screens/home_screen.dart';  // Import your HomeScreen
-import 'providers/user_data_provider.dart';  // Import the SelectedFoodProvider
+import 'screens/home_screen.dart'; // Import your HomeScreen
+import 'providers/user_data_provider.dart'; // Import the SelectedFoodProvider
 
 void main() async {
   await dotenv.load(fileName: 'assets/.env');
@@ -21,11 +22,13 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => UserDataProvider(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Food Selection App',
         initialRoute: '/',
         routes: {
           '/': (context) => const HomeScreen(),
-          '/results': (context) => const ResultsScreen(results: []),
+          '/results': (context) => const ResultsScreen(
+              results: [], screenType: ResultScreenType.manual),
           '/scan': (context) => const ScanFoodScreen(),
         },
       ),
